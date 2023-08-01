b365393678e47e031285d77cc0e8f9cb67de9222
Eren OLKUN <erenolkun@gmail.com>
2023-08-01 16:42:48 +0300

    Initial Commit

---
+++ JokesAppSwiftUI/JokesAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppSwiftUIApp.swift
+//  JokesAppSwiftUI
+//
+//  Created by FERDA OLKUN on 1.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
